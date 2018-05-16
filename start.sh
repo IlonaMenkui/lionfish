00d652dd7f531b9c92c7e1645a96deb15464ef03
ilonaspolding@gmail.com <aliaksandr.babai@gmail.com>
2018-05-16 12:54:16 +0300

    refactor(startup): improve script

--- start.sh
+++ start.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-export LF_SERVER_PORT=8080
+LF_SERVER_PORT=8080
 
-NGROK_COMMAND="ngrok http $LF_SERVER_PORT"
+while [[ $# -ge 1 ]]
+do
+key="$1"
+case "$1" in
+    -p|--port) LF_SERVER_PORT=$2;;
+    -t|--token) LF_APP_BOT_TOKEN=$2;;
+    *)
+    printf "\nUnknown command!\n\n"
+    exit 1
+    ;;
+esac
+shift # past argument or value
+done
 
-if [ `jobs | grep "$NGROK_COMMAND" | wc -l` -eq 0 ]; then
-    $NGROK_COMMAND &
+SSH_TUNNEL_COMMAND="ngrok http $LF_SERVER_PORT"
+OPERATION_TIMEOUT=3
+
+if [ `jobs | grep "$SSH_TUNNEL_COMMAND" | wc -l` -eq 0 ]; then
+    "${SSH_TUNNEL_COMMAND}" &>/dev/null;
 fi
 
-export LF_LB_URL=`curl --silent --show-error --max-time 3 http://127.0.0.1:4040/api/tunnels | grep -Eoh "http://.*?ngrok.io"`
+export LF_SERVER_PORT
+export LF_LB_URL=`timeout -k 3 $OPERATION_TIMEOUT curl --silent --show-error http://127.0.0.1:4040/api/tunnels | grep -Eoh "http://.*?ngrok.io"`
 
-yarn start 
+yarn && yarn start
