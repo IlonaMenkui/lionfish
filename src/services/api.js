6011e0332afd1526388f83b67c5a5f4849d1368a
MenkuiChan <ilonaspolding@gmail.com>
2018-05-30 03:02:52 +0300

    feat(bot): implement random anime by genre

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,17 @@
 const axios = require('axios')
+const _ = require('lodash')
 
 module.exports = class Api {
     constructor({ baseUrl }) {
         this.axios = axios.create({
             baseURL: baseUrl
         })
+    }
+
+    getGenres() {
+        return this.axios
+            .get('/api/genres')
+            .then(res => res.data)
     }
 
     find(options = {}) {
@@ -20,6 +27,16 @@ module.exports = class Api {
             order: 'popularity',
             ...options
         })
+    }
+
+    findRandomByGenre(genre, options = {}) {
+        return this.find({
+            genre: genre.toString(),
+            limit: 50,
+            order: 'random',
+            ...options
+        })
+        .then(_.sample)
     }
 
     findAnimeByName(name, options = {}) {
--- src/services/bot.js
+++ src/services/bot.js
@@ -1,8 +1,11 @@
 const Telegraf = require('telegraf')
+const Markup = require('telegraf/markup')
 const session = require('telegraf/session')
 const Stage = require('telegraf/stage')
 const Scene = require('telegraf/scenes/base')
 const { leave } = Stage
+
+const _ = require('lodash')
 
 module.exports = class Bot {
     constructor({ token, webhookUrl, dependencies: { api, templateEngine } }) {
@@ -34,18 +37,40 @@ module.exports = class Bot {
         topScene.hears(/^(19[5-9]\d|20[0-4]\d|2050)$/, leave())
         topScene.on('message', (ctx) => ctx.reply('Пожалуйста, введите год'))
 
+        const randScene = new Scene('rand')
+        randScene.enter(async ctx => {
+            const genres = await this.api.getGenres()
+            const randomGenres = _.sampleSize(genres, 10)
+            const keys = _
+                .chunk(randomGenres, 3)
+                .map(chunk => chunk
+                    .map(genre => Markup.callbackButton(genre.russian, `genre-${genre.id}`)))
+            ctx.reply(
+                'Пожалуйста, выберите жанр',
+                Markup.inlineKeyboard(keys).extra()
+            )
+        })
+        randScene.leave(async ctx => {
+            const genreId = ctx.update.callback_query.data.split('-')[1]
+            const anime = await this.api.findRandomByGenre(genreId)
+            ctx.deleteMessage()
+            return ctx.reply(
+                `[${anime.russian}](${this.api.expandUrl(anime.url)})`,
+                { parse_mode: 'Markdown' }
+            )
+        })
+        randScene.on('callback_query', leave())
+        randScene.on('message', (ctx) => ctx.reply('Пожалуйста, выберите жанр'))
+
         stage.register(topScene)
+        stage.register(randScene)
 
         this.telegraf.use(session())
         this.telegraf.use(stage.middleware())
 
         this.telegraf.command('top', (ctx) => ctx.scene.enter('top'))
-        this.telegraf.command('top', (ctx) => ctx.scene.enter('random'))
+        this.telegraf.command('rand', (ctx) => ctx.scene.enter('rand'))
         this.telegraf.command('cancel', leave())
-
-        this.telegraf.command('ping', async ctx => {
-            return ctx.reply('pong')
-        })
 
         this.telegraf.on('inline_query', async ctx => {
             const limit = 20
@@ -57,7 +82,8 @@ module.exports = class Bot {
                     type: 'article',
                     title: anime.russian,
                     input_message_content: {
-                        message_text: this.api.expandUrl(anime.url)
+                        message_text: `[${anime.russian}](${this.api.expandUrl(anime.url)})`,
+                        parse_mode: 'Markdown'
                     }
                 }))
             return ctx.answerInlineQuery(results, {
